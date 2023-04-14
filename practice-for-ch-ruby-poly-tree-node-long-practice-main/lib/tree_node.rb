b19e721f4a63eb50e3646b0d44c656051921c5e9
Matthew Montejo <montejododger@yahoo.com>
2023-04-14 15:22:56 -0700

    Treenode frame work

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,33 @@
 class PolyTreeNode
-    #this is a test
+    
+    attr_reader :parent, :children, :value
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+    
+    def parent=(other_node)
+        @parent = other_node
+        
+        other_node.parent = 
+
+        if @parent.nil?
+
+        elsif !other_node.children.include?(self)
+            other_node.children << self
+        end
+
+    end
+    
+    def add_child
+        
+    end
+    
+    def remove_child
+        
+    end
+    
+    
 end
