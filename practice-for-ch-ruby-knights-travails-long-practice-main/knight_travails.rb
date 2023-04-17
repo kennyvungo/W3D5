b8059828982a7900b9a9dede894c80cdad13d476
kennyvngo <kennyvungo@gmail.com>
2023-04-17 11:13:48 -0700

    generate 8 possible positions and select the valid ones

--- practice-for-ch-ruby-knights-travails-long-practice-main/knight_travails.rb
+++ practice-for-ch-ruby-knights-travails-long-practice-main/knight_travails.rb
@@ -1,24 +1,30 @@
-require './tree_node.rb'
+require '../practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb'
 require 'byebug'
 
 class KnightPathFinder
 
     attr_reader :root_node
     def initialize(start_pos)
-        # @start_pos = start_pos
+        @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
-
-        self.build_move_tree
+        # self.build_move_tree
     end
 
     def self.valid_moves(pos)
         r, c = pos
-        posible_pos = []
-        [r+1, r+2, r-1, r-2].each do |n_row|
-            [c+1, c+2, c-1, c-2].each do |n_col|
-                
-            end
-        end
+        possible_pos = []
+        #generate 8 possible positions 
+        possible_pos << [r+2,c+1]
+        possible_pos << [r+2,c-1]
+        possible_pos << [r-2,c+1]
+        possible_pos << [r-2,c-1]
+        possible_pos << [r+1,c+2]
+        possible_pos << [r-1,c-2]
+        possible_pos << [r+1,c-2]
+        possible_pos << [r-1,c+2]
+        #check which positions are valid
+        valids = possible_pos.select{|indices|  indices[0].between?(0,7) && indices[1].between?(0,7)}
+        valids
     end
 
     def build_move_tree(node)
@@ -28,7 +34,7 @@ class KnightPathFinder
 end
 
 if $PROGRAM_NAME == __FILE__
-    p kpf = KnightPathFinder([])
+    p kpf = KnightPathFinder.new([0,0])
 
-    p kpf.
+    p KnightPathFinder.valid_moves([7,7])
 end
